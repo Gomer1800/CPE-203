141c5da38a47c9816da88ebdd3ef636f4490dfd1
lgomez27 <luisgomez1800@gmail.com>
2018-10-02 12:45:51 -0700

    started hw1 & lab2

---
+++ lab2/part1/Polygon.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Polygon {
+   private ArrayList<Point> vertices;
+
+   public Polygon(List<Point> points) {
+      vertices = new ArrayList<Point>();
+      for (int i=0; i<points.size(); i++) {
+         vertices.add(points.get(i));
+      }
+   }
+} 
