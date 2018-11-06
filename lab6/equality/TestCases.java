da5dd429c1e840030463ba76aef33ee83e7748c5
lgomez27 <luisgomez1800@gmail.com>
2018-11-06 09:07:22 -0800

    completed Student equals and hashcode tests successfully

--- lab6/equality/TestCases.java
+++ lab6/equality/TestCases.java
@@ -17,7 +17,7 @@ public class TestCases
    @Test
    public void testExercise1()
    {
-       System.out.println("\nTESTING COURSESECTION");
+       System.out.println("\nTESTING COURSESECTION EQUALS");
       final CourseSection one = new CourseSection("CSC", "203", 35,
          LocalTime.of(9, 40), LocalTime.of(11, 0));
       final CourseSection two = new CourseSection("CSC", "203", 35,
@@ -30,7 +30,7 @@ public class TestCases
    @Test
    public void testExercise2()
    {
-       System.out.println("\nTESTING COURSESECTION");
+       System.out.println("\nTESTING COURSESECTION EQUALS");
 
       final CourseSection one = new CourseSection("CSC", "203", 35,
          LocalTime.of(9, 10), LocalTime.of(10, 0));
@@ -44,7 +44,7 @@ public class TestCases
    @Test
    public void testExercise3()
    {
-       System.out.println("\nTESTING COURSESECTION");
+       System.out.println("\nTESTING COURSESECTION HASHCODE");
 
       final CourseSection one = new CourseSection("CSC", "203", 35,
          LocalTime.of(9, 40), LocalTime.of(11, 0));
@@ -57,7 +57,7 @@ public class TestCases
    @Test
    public void testExercise4()
    {
-       System.out.println("\nTESTING COURSESECTION");
+       System.out.println("\nTESTING COURSESECTION HASHCODE");
 
       final CourseSection one = new CourseSection("CSC", "203", 35,
          LocalTime.of(9, 10), LocalTime.of(10, 0));
@@ -69,7 +69,7 @@ public class TestCases
 
    @Test
    public void testExcercise5() {
-       System.out.println("\nTESTING STUDENT");
+       System.out.println("\nTESTING STUDENT EQUALS");
        // courses of student 1
        List<CourseSection> currentCourses1 =  new ArrayList<>();
        currentCourses1.add(new CourseSection("CSC", "203", 35,
@@ -90,4 +90,27 @@ public class TestCases
        assertTrue(student2.equals(student1));
 
    }
+
+   @Test
+   public void testExcercise6() {
+       System.out.println("\nTESTING STUDENT HASHCODE");
+       // courses of student 1
+       List<CourseSection> currentCourses1 =  new ArrayList<>();
+       currentCourses1.add(new CourseSection("CSC", "203", 35,
+         LocalTime.of(9, 10), LocalTime.of(10, 0))) ;
+       currentCourses1.add(new CourseSection("CSC", "203", 34,
+         LocalTime.of(9, 10), LocalTime.of(10, 0))) ;
+       // courses of student 2
+       List<CourseSection> currentCourses2 =  new ArrayList<>();
+       currentCourses2.add(new CourseSection("CSC", "203", 35,
+         LocalTime.of(9, 10), LocalTime.of(10, 0))) ;
+       currentCourses2.add(new CourseSection("CSC", "203", 34,
+         LocalTime.of(9, 10), LocalTime.of(10, 0))) ;
+
+       Student student1 = new Student("Gomez", "Luis", 27, currentCourses1 ) ;
+       Student student2 = new Student("Gomez", "Luis", 27, currentCourses2 ) ;
+       
+       assertEquals( student1.hashCode(), student2.hashCode());
+   }
+
 }
